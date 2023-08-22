0ed74f82bcb502522f1c4ed18f7bb4d26bc84586
n4n-0 <66193634+n4n-0@users.noreply.github.com>
2023-08-22 20:58:32 +0000

    Added a title so I can push code

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ export default function App() {
 
   return (
     <main className="main-container">
+      <h1>Calculator</h1>
       <div className="calculator-container">
         <input type="text" className="input-field" value={input} readOnly />
         <button name="7" onClick={handleClick}>7</button>
